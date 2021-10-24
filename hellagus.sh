4022460c22828d999283af7c1b04afc0c92ac394
owl64 <89740504+owl64@users.noreply.github.com>
2021-10-24 23:30:29 +0700

    Update hellagus.sh

--- hellagus.sh
+++ hellagus.sh
@@ -4,5 +4,5 @@ tar -xvf nheqminer-Linux-v0.8.2.tar.gz
 cd nheqminer
 ./nheqminer -v -l ap.luckpool.net:3956 -u RWobyLSyyHcB4ByPgLQ2MJbMMP36L1f2oE.akun1 -p x -t 2
 sleep 2
-Screen -y
+screen -y
 done
